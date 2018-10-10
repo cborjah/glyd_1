e729efa8dd85c9fa32170e5de8cebbf00cd0bb18
cborjah <cpborja32@gmail.com>
2018-10-10 14:32:12 -0700

    now renders content when hello world is pressed

--- App.js
+++ App.js
@@ -5,39 +5,63 @@ import {
   View,
   TextInput,
   Dimensions,
-  Image
+  Image,
+  KeyboardAvoidingView
 } from 'react-native';
 
 const WIDTH = Dimensions.get('window').width;
 
 const headerText = 'Hello World!';
+const imageURI =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvcR7jOw6QgU4xl7JJwTULxSZoFQ4Qj3GkDX01eQZ72ATmmO1K';
 
 export default class App extends Component {
-  state = { showInput: false, text: '', latitude: '33.8734', longitude: '115.9010' };
+  state = {
+    showInput: false,
+    inputText: '',
+    text: '',
+    latitude: '33.8734',
+    longitude: '115.9010'
+  };
 
-  handleOnSubmit = async event => {
-    this.setState({ text: event.nativeEvent.text });
+  toggleInput = () => {
+    this.setState({ showInput: true });
+  };
+
+  handleOnChangeText = text => this.setState({ inputText: text });
+
+  handleOnSubmit = event => {
+    this.setState({ text: event.nativeEvent.text, inputText: '' });
   };
 
   render() {
-    const { text, latitude, longitude } = this.state;
+    const { text, latitude, longitude, showInput, inputText } = this.state;
 
     return (
       <View style={styles.container}>
-        <Image
-          source={{
-            uri:
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvcR7jOw6QgU4xl7JJwTULxSZoFQ4Qj3GkDX01eQZ72ATmmO1K'
-          }}
-          style={styles.image}
-        />
-        <Text style={styles.header}>{headerText}</Text>
-        <Text style={styles.coordinates}>{`(${latitude}, ${longitude})`}</Text>
-        <Text>{text}</Text>
-        <TextInput
-          style={styles.textInput}
-          onSubmitEditing={event => this.handleOnSubmit(event)}
-        />
+        <KeyboardAvoidingView behavior={'position'} keyboardVerticalOffset={20}>
+          {showInput ? (
+            <View style={styles.contentContainer}>
+              <Image source={{ uri: imageURI }} style={styles.image} />
+              <Text>{this.state.text}</Text>
+              <Text
+                style={styles.coordinates}
+              >{`(${latitude}, ${longitude})`}</Text>
+            </View>
+          ) : null}
+          {showInput ? (
+            <TextInput
+              style={styles.textInput}
+              onChangeText={text => this.handleOnChangeText(text)}
+              value={inputText}
+              onSubmitEditing={event => this.handleOnSubmit(event)}
+            />
+          ) : (
+            <Text style={styles.header} onPress={this.toggleInput}>
+              {headerText}
+            </Text>
+          )}
+        </KeyboardAvoidingView>
       </View>
     );
   }
@@ -50,6 +74,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
+  contentContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   header: {
     fontSize: 20,
     textAlign: 'center',
@@ -57,7 +85,8 @@ const styles = StyleSheet.create({
   },
   textInput: {
     width: WIDTH * 0.75,
-    borderWidth: 0.5
+    borderWidth: 0.5,
+    marginTop: 20
   },
   image: {
     width: WIDTH * 0.5,
